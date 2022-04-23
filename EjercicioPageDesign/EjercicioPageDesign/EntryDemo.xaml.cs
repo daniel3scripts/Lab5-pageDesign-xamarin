8a205d896aec826b5851c6076761b919c6f2e073
daniel3scripts <paul.lugo@tecsup.edu.pe>
2022-04-22 19:07:41 -0500

    commit

---
+++ EjercicioPageDesign/EjercicioPageDesign/EntryDemo.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EjercicioPageDesign
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryDemo : ContentPage
+    {
+        public EntryDemo()
+        {
+            InitializeComponent();
+           
+        }
+    }
+}
---
+++ EjercicioPageDesign/EjercicioPageDesign/LabelCode.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EjercicioPageDesign
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LabelCode : ContentPage
+    {
+        public LabelCode()
+        {
+            InitializeComponent();
+            StackLayout stack = new StackLayout();
+            var underlineLabel = new Label { Text = "This is underlined text.",
+                TextDecorations = TextDecorations.Underline,FontSize=30 };
+            
+            var strikethroughLabel = new Label { Text = "This is text with strikethrough.",
+                TextDecorations=TextDecorations.Strikethrough,
+                FontSize = 20
+            };
+            var bothLabel=new Label { Text = "This is underlined text with strikethrough",
+                TextDecorations = TextDecorations.Underline | TextDecorations.Strikethrough,
+                FontSize = 20
+            };
+
+            stack.Children.Add(underlineLabel);
+            stack.Children.Add(strikethroughLabel);
+            stack.Children.Add(bothLabel);
+            Content = stack;
+        
+        }
+    }
+}
